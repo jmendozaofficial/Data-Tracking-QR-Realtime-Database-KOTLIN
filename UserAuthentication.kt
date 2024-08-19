f3e40f74278e70cb5fb56efe32ce046e400c2037
Janrae Mendoza <jmendoza.eps@gmail.com>
2024-08-19 13:16:51 +0800

    Create UserAuthentication.kt
    This is how to authenticate the user and the password thrown in the mobile app. (this is now unused as it is unnecessary to have tons of users for a qr scanner app that does the same build over and over again.

---
+++ UserAuthentication.kt
@@ -0,0 +1,29 @@
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UserAuthentication : AppCompatActivity() {
+    lateinit var usernameInput : EditText
+    lateinit var passwordInput : EditText
+    lateinit var loginBtn : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_user_authentication)
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginBtn = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            
+        }
+
+    }
